095478711051f7abc5ae6ffd943b4c901ace9774
vaibhav1908875 <vaibhavkumawat2016vk@gmail.com>
2023-02-24 16:03:02 +0530

    End of section 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+        public RegionsController(IRegionRepository regionRepository,IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions =await regionRepository.GetAllAsync();
+            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
+            Console.WriteLine(regionsDTO);
+            return Ok(regionsDTO);
+
+            ////return DTO regions(from database)
+
+            //var regionsDTO = new List<Models.DTO.Region>();
+            //regions.ToList().ForEach(region =>
+            //{
+            //    var regionDTO = new Models.DTO.Region()
+            //    {
+            //        Id = region.Id,
+            //        Code  = region.Code,
+            //        Name = region.Name,
+            //        Area = region.Area,
+            //        Lat = region.Lat,
+            //        Long = region.Long,
+            //        Population = region.Population,
+            //    };
+            //    regionsDTO.Add(regionDTO);
+            //});
+            //return Ok(regionsDTO);
+
+
+
+
+
+            //static data
+
+            //var regions = new List<Region>()
+            //{
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Wellington",
+            //        Code = "WLG",
+            //        Area=226654,
+            //        Lat = -6.4654,
+            //        Long = 56.87,
+            //        Population = 500000
+            //    },
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Auckland",
+            //        Code = "AUCK",
+            //        Area=456745,
+            //        Lat = -62.4654,
+            //        Long = 34.8787,
+            //        Population = 700000
+            //    }
+            //};
+
+        }
+    }
+}
